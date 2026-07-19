2be204062748eea209598a7b8ab494004d4a8f08
JoshuaMSolo <87811513+JoshuaMSolo@users.noreply.github.com>
2026-07-20 04:03:04 +0700

    Add: middle-of-the-linked-list - submission-1

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-1.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast != None :
+            if fast.next != None :
+                fast = fast.next
+            else :
+                return slow
+            
+            slow = slow.next
+
+            if fast.next != None :
+                fast = fast.next
+            else :
+                return slow
