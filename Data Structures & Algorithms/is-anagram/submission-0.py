a1caabe0e3847efc23a8eff8ac37ff02f9db7ab6
JoshuaMSolo <87811513+JoshuaMSolo@users.noreply.github.com>
2026-07-22 15:43:33 +0700

    Bulk sync: 22 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+        for char in s :
+            count[char] += 1
+        for char in t :
+            count[char] -= 1
+        for c in count.values() :
+            if c != 0 :
+                return False
+        return True
