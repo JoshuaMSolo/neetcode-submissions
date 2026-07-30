d0ac0f9e51f6ed7a4df4c1da3e0136d8c2625f5d
JoshuaMSolo <87811513+JoshuaMSolo@users.noreply.github.com>
2026-07-30 15:11:33 +0700

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        toRep = [i for i in range(n)]
+
+        def findRep(i) :
+            if toRep[i] == i:
+                return i
+            else :
+                toRep[i] = findRep(toRep[i])
+                return toRep[i]
+
+        res = n
+        for a, b in edges :
+            a_rep, b_rep = findRep(a), findRep(b)
+            if a_rep != b_rep:
+                res -= 1
+                toRep[b_rep] = a_rep
+        
+        return res
